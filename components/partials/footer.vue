d969a9c0b060515f4a5776bc453c6239f381fc7f
Putnug1122 <asiapatta10@gmail.com>
2021-09-28 20:01:08 +0800

    base

---
+++ components/partials/footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <footer class="main-footer">
+      <div class="footer-left">
+        Copyright &copy; 2021
+        <div class="bullet"></div>
+        Created By <a href="https://nauval.in/">Kelompok 3</a>
+      </div>
+      <div class="footer-right">2.3.0</div>
+    </footer>
+  </div>
+</template>
---
+++ components/partials/sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="main-sidebar">
+      <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+          <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+          <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+          <li class="menu-header">Tugas</li>
+          <li class="nav-item" v-for="(menu, i) in listClass" :key="i">
+            <nuxt-link class="nav-link" :to="menu.link"
+              ><i :class="menu.icon"></i> <span>{{ menu.name }}</span>
+            </nuxt-link>
+          </li>
+        </ul>
+        <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+          <a
+            href="https://getstisla.com/docs"
+            class="btn btn-primary btn-lg btn-block btn-icon-split"
+          >
+            <i class="fas fa-rocket"></i> Documentation
+          </a>
+        </div>
+      </aside>
+    </div>
+  </div>
+</template>
