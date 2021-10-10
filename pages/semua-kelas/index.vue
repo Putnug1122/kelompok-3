7f2459ba3c711f41d4664aa0376b45781fee651e
Putnug1122 <asiapatta10@gmail.com>
2021-10-10 12:44:38 +0800

    hehe

--- pages/semua-kelas/index.vue
+++ pages/semua-kelas/index.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>
-    <card-item class="shadow" />
+  <div class="row">
+    <div v-for="(kelas, i) in listKelas" :key="i" class="m-4">
+      <card-item
+        class="shadow"
+        :id="kelas.id"
+        :name="kelas.name"
+        :start="kelas.start_date"
+        :end="kelas.end_date"
+      />
+    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {}
+    return {
+      listKelas: [
+        {
+          id: 1,
+          name: 'Front End',
+          start_date: '19 Agustus 2021',
+          end_date: '19 Desember 2021',
+        },
+        {
+          id: 2,
+          name: 'Back End',
+          start_date: '19 Agustus 2021',
+          end_date: '19 Desember 2021',
+        },
+        {
+          id: 3,
+          name: 'Full Stack',
+          start_date: '19 Agustus 2021',
+          end_date: '19 Desember 2021',
+        },
+      ],
+    }
   },
 }
 </script>
